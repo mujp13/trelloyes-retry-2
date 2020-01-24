cc1eee978778aa89394970fab09fd693a06bd242
Kevin Park <keunsuk413@gmail.com>
2020-01-24 17:04:02 -0600

    2nd commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store'
 
 
 ReactDOM.render(
-  <App />, 
+  <App store={ store }/>, 
   document.getElementById('root')
 );
 
